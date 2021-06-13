8c9b4f05e01919a845324401b5272dc067efd9bf
kdaniilm <kdaniilm@gmail.com>
2021-06-13 12:49:16 +0300

    Add server Services

---
+++ SendFile/ServerBLL/Service/FileService.cs
@@ -0,0 +1,32 @@
+﻿using ServerBLL.Service.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerBLL.Service
+{
+    public class FileService : IFileService
+    {
+        public List<string> CreateLogPass()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveFile(byte[] fileBytesArr, string name, int size)
+        {
+            throw new NotImplementedException();
+        }
+
+        public byte[] SendFile(string name, int size)
+        {
+            throw new NotImplementedException();
+        }
+
+        public byte[] SendFile(string log, string pass)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SendFile/ServerBLL/Service/Interfaces/IFileService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerBLL.Service.Interfaces
+{
+    public interface IFileService
+    {
+        byte[] SendFile(string log, string pass);
+
+        void SaveFile(byte[] fileBytesArr, string name, int size);
+
+        List<string> CreateLogPass();
+    }
+}
